b4d5e1064fd5d73c0cc81d781f4cb99cc547a435
Marcos <marcos.gomez47@unach.mx>
2023-10-26 02:08:04 -0600

    Conexión BD Metodo Largo
    Hice la conexión a la base de datos con el método largo

--- InicioSesion.Repos/Services/LoginService.cs
+++ InicioSesion.Repos/Services/LoginService.cs
@@ -1,10 +1,60 @@
+using System.Data.SqlClient;
 using InicioSesion.Repos.Interfaces;
 
 public class LoginService : Ilogin
 {
     public async Task<bool> Login(User user)
-    {
-        return await Task.FromResult(true);
+    {   
+        // using (var conexion = new SqlConnection(ValoresEstaticos.ConexionDB))
+        // {
+        //     await conexion.OpenAsync();
+
+        //     SqlCommand cmd = new SqlCommand();
+        //     cmd.CommandText="[dbo].[ProcedimientoUsuarios]";    
+        // }
+
+
+    //Logica de la Conexion a la Base de Datos
+    bool resultado=false;
+        using (var conexion = new SqlConnection(ValoresEstaticos.ConexionDB))
+        {
+            try 
+            {
+                var Comando = new SqlCommand();
+                Comando.Connection=conexion;
+                Comando.CommandText= "[dbo].[ProcedimientoUsuarios]";
+                Comando.CommandType=System.Data.CommandType.StoredProcedure;
+                Comando.Parameters.AddWithValue("@Usuario",user.UserName);
+                Comando.Parameters.AddWithValue("@Password", user.Password);
+
+                await conexion.OpenAsync();
+                
+                var lectura = await Comando.ExecuteReaderAsync();
+
+                if(lectura.HasRows)
+                {
+                    if(lectura.Read())
+                    {   
+                        resultado = true;
+                        /*
+                        Resultado.IdEmpleado = lectura.GetGuid(0);
+                        Resultado.Nombre = lectura.GetGuid(1);
+                        Resultado.Nombre = lectura.GetGuid(2);
+                        */
+                    }else
+                    {
+                       resultado=false;
+                    }
+                }    
+            }        
+            catch(System.Exception)
+            {
+
+            }
+            
+        }
+
+        return resultado;
     }
     
 }
